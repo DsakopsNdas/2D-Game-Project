857a0d863900941fc0f465e2f6169a9d63de45c9
Daniel <daniel.nguyen8@mohawkcollege.ca>
2025-11-09 20:32:36 -0500

    Update Ball class
    Create constructor and I think it can move now?

--- nguyen-daniel-a3-project/Ball.cs
+++ nguyen-daniel-a3-project/Ball.cs
@@ -8,13 +8,40 @@ namespace MohawkGame2D
     public class Ball
     {
         //variables
-        int veloX = 1;
-        int veloY = 1;
+        int veloX = 0;
+        int veloY = 0;
         Vector2 initPos;
+        Vector2 velocity = Vector2.Zero;
+
+        public Ball()
+        {
+            bool upOrDown = Random.Bool();
+            bool rightOrLeft= Random.Bool();
+            
+            if (upOrDown == true)
+            {
+                veloY = 1;
+            }
+            else
+            {
+                veloY = -1;
+            }
+            if (rightOrLeft == true)
+            {
+                veloX = 1;
+            }
+            else
+            {
+                veloX = -1;
+            }
+            velocity = new Vector2(veloX, veloY);
+        }
+
         public void Balling()
         {
             initPos = new Vector2(Window.Width / 2, Window.Height / 2);
             Draw.Circle(initPos, 5);
+            initPos += velocity;
         }
     }
 }
