8f066f64188bd4a8a1d499d2c3cc23e1cd1adcb0
Daniel <daniel.nguyen8@mohawkcollege.ca>
2025-11-09 22:06:18 -0500

    Write some code for Paddle Class
    Trying to make everything scale perfectly with window size might've been a mistake but it's a mistake I'm committed to! I'm leaving the rest of this to tomorrow me!!!
    
    Also removed redundant system library call or something that was probably added by Intellisense.

--- nguyen-daniel-a3-project/Ball.cs
+++ nguyen-daniel-a3-project/Ball.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Numerics;
-using System.Security.Cryptography.X509Certificates;
 
 namespace MohawkGame2D
 {
--- nguyen-daniel-a3-project/Paddle.cs
+++ nguyen-daniel-a3-project/Paddle.cs
@@ -1,13 +1,41 @@
 ﻿using System;
+using System.Numerics;
 
 namespace MohawkGame2D
 {
 
 	public class Paddle
 	{
-		public Paddle()
+		public float[] cornerPositions = new float[4];
+		public bool firstPaddle = true;
+		
+		public void PaddleSetup()
 		{
-			//write paddle variables and functions here
+			if (firstPaddle)
+			{
+				cornerPositions =
+					[
+					Window.Width / 8, Window.Height / 4, //top left corner
+					Window.Width / 8 * 2, Window.Height / 4 * 2, //top right corner
+					Window.Width / 8 * 2, Window.Height / 4 * 2, //bottom right corner
+					Window.Width / 8 * 2, Window.Height / 4 * 2, //bottom left corner
+					];
+			}
+			else if (firstPaddle == false)
+			{
+                cornerPositions = 
+					[
+					Window.Width / 8 * -1 + Window.Width / 2, Window.Height / 4, //top left corner
+					Window.Width / 8 * 2 * -1 + Window.Width / 2, Window.Height / 4 * 2, //top right corner
+                    Window.Width / 8 * -1 + Window.Width / 2, Window.Height / 4, //bottom right corner
+                    Window.Width / 8 * 2 * -1 + Window.Width / 2, Window.Height / 4 * 2, //bottom left corner
+                    ];
+            }
 		}
+		public void Paddling()
+		{
+			//Draw.Quad(cornerPositions[1], cornerPositions[2], cornerPositions[3], cornerPositions[4]);
+
+        }
 	}
 }
