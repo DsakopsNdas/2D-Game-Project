32aa761cbb6b8335aab5ab8f07944048b5394a5b
Daniel <daniel.nguyen8@mohawkcollege.ca>
2025-11-11 23:04:52 -0500

    Fix ball array and ball-paddle collision
    Also tested the game at 300x100 and it works perfectly. Splendid.

--- nguyen-daniel-a3-project/Game.cs
+++ nguyen-daniel-a3-project/Game.cs
@@ -13,7 +13,7 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-        public Ball[] ball = new Ball[10];
+        public Ball[] balls;
         public Paddle paddle1 = new Paddle();
         public Paddle paddle2 = new Paddle();
         public Score score = new Score();
@@ -23,13 +23,15 @@ namespace MohawkGame2D
         /// </summary>
         public void Setup()
         {
+            balls = new Ball[10];
+            balls[0] = new Ball();
             Window.SetTitle("Pong.");
             Window.SetSize(600, 400);
             Window.TargetFPS = 60;
             Draw.FillColor = Color.White;
             Draw.LineColor = Color.White;
             Text.Color = Color.White;
-            ball[1].BallSetup();
+            balls[0].BallSetup();
             paddle1.PaddleSetup();
             paddle2.firstPaddle = false;
             paddle2.PaddleSetup();
@@ -37,18 +39,20 @@ namespace MohawkGame2D
 
         public bool Paddle1Collision()
         {
-            bool paddle1SideCollided = paddle1.cornerPositions[2] > ball[1].position.X - ball[1].radius;
-            bool paddle1LowerThanTopPoint = ball[1].position.Y > paddle1.cornerPositions[1];
-            bool paddle1HigherThanBottomPoint = ball[1].position.Y < paddle1.cornerPositions[5];
-            bool paddle1Collided = paddle1SideCollided && paddle1LowerThanTopPoint && paddle1HigherThanBottomPoint;
+            bool paddle1SideCollided = paddle1.cornerPositions[2] > balls[0].position.X - balls[0].radius;
+            bool paddle1NotPassed = paddle1.cornerPositions[0] < balls[0].position.X + balls[0].radius;
+            bool paddle1LowerThanTopPoint = balls[0].position.Y > paddle1.cornerPositions[1];
+            bool paddle1HigherThanBottomPoint = balls[0].position.Y < paddle1.cornerPositions[5];
+            bool paddle1Collided = paddle1SideCollided && paddle1NotPassed && paddle1LowerThanTopPoint && paddle1HigherThanBottomPoint;
             return paddle1Collided;
         }
         public bool Paddle2Collision()
         {
-            bool paddle2SideCollided = paddle2.cornerPositions[2] < ball[1].position.X + ball[1].radius;
-            bool paddle2LowerThanTopPoint = ball[1].position.Y > paddle2.cornerPositions[1];
-            bool paddle2HigherThanBottomPoint = ball[1].position.Y < paddle2.cornerPositions[5];
-            bool paddle2Collided = paddle2SideCollided && paddle2LowerThanTopPoint && paddle2HigherThanBottomPoint;
+            bool paddle2SideCollided = paddle2.cornerPositions[2] < balls[0].position.X + balls[0].radius;
+            bool paddle2NotPassed = paddle2.cornerPositions[0] > balls[0].position.X + balls[0].radius;
+            bool paddle2LowerThanTopPoint = balls[0].position.Y > paddle2.cornerPositions[1];
+            bool paddle2HigherThanBottomPoint = balls[0].position.Y < paddle2.cornerPositions[5];
+            bool paddle2Collided = paddle2SideCollided && paddle2NotPassed && paddle2LowerThanTopPoint && paddle2HigherThanBottomPoint;
             return paddle2Collided;
         }
 
@@ -59,26 +63,26 @@ namespace MohawkGame2D
         {
             Window.ClearBackground(Color.Black);
 
-            ball[1].Balling();
+            balls[0].Balling();
             paddle1.Paddling();
             paddle2.Paddling();
 
             if (Paddle1Collision() == true)
             {
-                ball[1].velocity.X *= -1;
+                balls[0].velocity.X *= -1;
             }
             if (Paddle2Collision() == true)
             {
-                ball[1].velocity.X *= -1;
+                balls[0].velocity.X *= -1;
             }
-            if (ball[1].position.X + ball[1].radius <= Window.Width / Window.Width - 1)
+            if (balls[0].position.X + balls[0].radius <= Window.Width / Window.Width - 1)
             {
-                ball[1].BallSetup();
+                balls[0].BallSetup();
                 score.score.Y++;
             }
-            if (ball[1].position.X - ball[1].radius >= Window.Width)
+            if (balls[0].position.X - balls[0].radius >= Window.Width)
             {
-                ball[1].BallSetup();
+                balls[0].BallSetup();
                 score.score.X++;
             }
 
